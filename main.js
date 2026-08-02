3eab143fb3693bafef638d69fd41ec6ce16eb81a
evan-duevel <evanjduevel@protonmail.com>
2026-08-01 19:13:49 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 export default async ({ req, res, log }) => {
+  if (req.method === "OPTIONS") {
+    return res.send("", 204, {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Methods": "POST, OPTIONS"
+    });
+  }
+
   log("Function executed");
 
-  return res.json({
-    message: "Hello from Appwrite Cloud Function!"
-  });
+  return res.json(
+    { message: "Hello from Appwrite Cloud Function!" },
+    200,
+    {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Methods": "POST, OPTIONS"
+    }
+  );
 };
